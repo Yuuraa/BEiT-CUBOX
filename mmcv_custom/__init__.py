2c1c2063c124403094d9939c86b1eac101661915
Yuuraa <yoorachoi8937@gmail.com>
2022-03-02 00:04:08 +0900

    Add MixedImages Dataset

--- mmcv_custom/__init__.py
+++ mmcv_custom/__init__.py
@@ -7,5 +7,6 @@ from .apex_runner.optimizer import DistOptimizerHook
 from .train_api import train_segmentor
 from .cubox_dataset import CUBOXDataset
 from .cubox_dataset_map import CUBOXInstanceDataset
+from .mix_dataset import MultiImageMixDataset
 
-__all__ = ['load_checkpoint', 'LayerDecayOptimizerConstructor', 'SETR_Resize', 'DistOptimizerHook', 'train_segmentor', 'CUBOXDataset', 'CUBOXInstanceDataset']
+__all__ = ['load_checkpoint', 'LayerDecayOptimizerConstructor', 'SETR_Resize', 'DistOptimizerHook', 'train_segmentor', 'CUBOXDataset', 'CUBOXInstanceDataset', 'MultiImageMixDataset']
